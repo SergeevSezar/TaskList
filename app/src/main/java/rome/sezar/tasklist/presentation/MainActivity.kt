180de526b534280228fb24100de59987761cfcf1
SergeevSezar <sergeev.sezar@yandex.ru>
2022-06-29 06:45:14 +0900

    add viewmodel in presentation layer

--- app/src/main/java/rome/sezar/tasklist/presentation/MainActivity.kt
+++ app/src/main/java/rome/sezar/tasklist/presentation/MainActivity.kt
@@ -2,11 +2,21 @@ package rome.sezar.tasklist.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import rome.sezar.tasklist.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        viewModel.taskList.observe(this) {
+            TODO()
+        }
+        viewModel.getTaskList()
     }
 }
---
+++ app/src/main/java/rome/sezar/tasklist/presentation/MainViewModel.kt
@@ -0,0 +1,33 @@
+package rome.sezar.tasklist.presentation
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import rome.sezar.tasklist.data.DailyTaskRepositoryImpl
+import rome.sezar.tasklist.domain.AddDailyTaskUseCase
+import rome.sezar.tasklist.domain.DailyTask
+import rome.sezar.tasklist.domain.GetAllDailyTaskListUseCase
+import rome.sezar.tasklist.domain.RemoveDailyTaskUseCase
+
+class MainViewModel : ViewModel() {
+
+    private val repository: DailyTaskRepositoryImpl = DailyTaskRepositoryImpl
+
+    private val getAllDailyTaskListUseCase: GetAllDailyTaskListUseCase = GetAllDailyTaskListUseCase(repository)
+    private val addDailyTaskUseCase: AddDailyTaskUseCase = AddDailyTaskUseCase(repository)
+    private val removeDailyTaskUseCase: RemoveDailyTaskUseCase = RemoveDailyTaskUseCase(repository)
+
+    val taskList = MutableLiveData<List<DailyTask>>()
+
+    fun getTaskList() {
+        val list = getAllDailyTaskListUseCase.getDailyTaskList()
+        taskList.value = list
+    }
+
+    fun addDailyTask(dailyTask: DailyTask) {
+        addDailyTaskUseCase.addDailyTask(dailyTask)
+    }
+
+    fun removeDailyTask(dailyTask: DailyTask) {
+        removeDailyTaskUseCase.removeDailyTask(dailyTask)
+    }
+}
